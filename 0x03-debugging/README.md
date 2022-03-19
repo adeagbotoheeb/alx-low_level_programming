b607cced83f07d2748c933f6b6efa282e3bae445
adeagbotoheeb <adeagbotoheeb1@gmail.com>
2022-03-19 10:47:41 -0700

    debugging task 2

--- 0x03-debugging/README.md
+++ 0x03-debugging/README.md
@@ -1,1 +1,1 @@
-This lesson is all about finding and crushing bugs in code. We will learn about rubberduck debugging, printf debugging, and write tests for our code. Good luck engineer!!!
+#We're just starting Debugging in C
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,89 +1,11 @@
-#include "main.h"/*** main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
+#ifndef MAIN_H
+#define MAIN_H
 
-int main(void)
-{
-        int i;
+#include <stdio.h>
 
-        i = 0;
-        positive_or_negative(i);
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
-        return (0);
-}
-root@3dadedda038e:~/alx-low_level_programming/0x03-debugging# vi main.h
-root@3dadedda038e:~/alx-low_level_programming/0x03-debugging# cat
-
-root@3dadedda038e:~/alx-low_level_programming/0x04-more_functions_nested_loops# cd ..
-root@3dadedda038e:~/alx-low_level_programming# ls
-0x00-hello_world              0x02-functions_nested_loops  0x04-more_functions_nested_loops
-0x01-variables_if_else_while  0x03-debugging               README.md
-root@3dadedda038e:~/alx-low_level_programming# cat README.md
-low level programming(C)
-functions, nested loop
-Debugging in C
-root@3dadedda038e:~/alx-low_level_programming# vi README.md
-root@3dadedda038e:~/alx-low_level_programming# cd 0x03-debugging/
-root@3dadedda038e:~/alx-low_level_programming/0x03-debugging# ls
-0-main.c  main.h  positive_or_negative.c  README.md
-root@3dadedda038e:~/alx-low_level_programming/0x03-debugging# rm positive_or_negative.c
-root@3dadedda038e:~/alx-low_level_programming/0x03-debugging# ls
-0-main.c  main.h  README.md
-root@3dadedda038e:~/alx-low_level_programming/0x03-debugging# cat main.h
-cat > main.h
-#include "main.h"
-
-/**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
-
-int main(void)
-{
-        int i;
-
-        i = 0;
-        positive_or_negative(i);
-
-        return (0);
-}
-root@3dadedda038e:~/alx-low_level_programming/0x03-debugging# vi main.h
-root@3dadedda038e:~/alx-low_level_programming/0x03-debugging# cat
-root@3dadedda038e:~/alx-low_level_programming/0x04-more_functions_nested_loops# cd ..
-root@3dadedda038e:~/alx-low_level_programming# ls
-0x00-hello_world              0x02-functions_nested_loops  0x04-more_functions_nested_loops
-0x01-variables_if_else_while  0x03-debugging               README.md
-root@3dadedda038e:~/alx-low_level_programming# cat README.md
-low level programming(C)
-functions, nested loop
-Debugging in C
-root@3dadedda038e:~/alx-low_level_programming# vi README.md
-root@3dadedda038e:~/alx-low_level_programming# cd 0x03-debugging/
-root@3dadedda038e:~/alx-low_level_programming/0x03-debugging# ls
-0-main.c  main.h  positive_or_negative.c  README.md
-root@3dadedda038e:~/alx-low_level_programming/0x03-debugging# rm positive_or_negative.c
-root@3dadedda038e:~/alx-low_level_programming/0x03-debugging# ls
-0-main.c  main.h  README.md
-root@3dadedda038e:~/alx-low_level_programming/0x03-debugging# cat main.h
-cat > main.h
-#include "main.h"
-
-/**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
-
-int main(void)
-{
-        int i;
-
-        i = 0;
-        positive_or_negative(i);
-
-        return (0);
-}
-root@3dadedda038e:~/alx-low_level_programming/0x03-debugging# vi main.h
-root@3dadedda038e:~/alx-low_level_programming/0x03-debugging# cat
-0-main.c
-0-main.c
-cd 0-main.c
+#endif /* MAIN_H */
