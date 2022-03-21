f50ee229381964a1579bec1f75d8367cbc36795c
adeagbotoheeb <adeagbotoheeb1@gmail.com>
2022-03-21 14:48:02 -0700

    first task on pointer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,9 +3,10 @@
 /**
  * reset_to_98-update the value *n points to.
  * @n: the number to modify.
+ * Return: no return.
  */
 
-void reset_to_98(int*a)
+void reset_to_98(int *n)
 {
-	*n=98;
-}	
+	*n = 98;
+}
